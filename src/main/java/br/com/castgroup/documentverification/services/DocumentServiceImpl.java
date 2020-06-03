11a102fb149e080e25adede3abe4ea8923b4aa7c
nfbf <45438873+nfbf@users.noreply.github.com>
2020-06-03 18:25:29 -0300

    implementação do service e classes utils

--- src/main/java/br/com/castgroup/documentverification/services/DocumentServiceImpl.java
+++ src/main/java/br/com/castgroup/documentverification/services/DocumentServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import br.com.castgroup.documentverification.interfaces.DocumentService;
 import br.com.castgroup.documentverification.model.Document;
 import br.com.castgroup.documentverification.repositories.DocumentRepository;
+import br.com.castgroup.documentverification.utils.ConstantsUtil;
+import br.com.castgroup.documentverification.utils.VerifyDocumentUtil;
 
 @Service
 public class DocumentServiceImpl implements DocumentService {
@@ -16,7 +18,7 @@ public class DocumentServiceImpl implements DocumentService {
 	private DocumentRepository documentRepository;
 
 	public void saveDocument(Document document) {
-		Document searchDocument = findDocument(document);
+		Document searchDocument = findDocument(document.getId());
 
 		if (searchDocument == null) {
 			documentRepository.save(document);
@@ -31,17 +33,24 @@ public class DocumentServiceImpl implements DocumentService {
 
 	}
 
-	private Document findDocument(Document document) {
-		Optional<Document> searchDocument = documentRepository.findById(document.getId());
+	public String compareDocument(long id) {
+		Document compareDocument = findDocument(id);
+
+		if (compareDocument != null) {
+
+			String compareDocLeft = VerifyDocumentUtil.decodeDataBase64(compareDocument.getDocLeft());
+			String compareDocRight = VerifyDocumentUtil.decodeDataBase64(compareDocument.getDocRight());
+
+			return VerifyDocumentUtil.resultCompareDocument(compareDocLeft, compareDocRight);
+
+		} else {
+			return ConstantsUtil.DOCUMENT_NOT_FOUND;
+		}
+	}
+
+	private Document findDocument(long id) {
+		Optional<Document> searchDocument = documentRepository.findById(id);
 		return searchDocument.isPresent() ? searchDocument.get() : null;
 	}
-	
-	public String compareDocument (long id) {
-		
-		return null;
-	}
-	
-	
-	
 
 }
--- src/main/java/br/com/castgroup/documentverification/utils/VerifyDocumentUtil.java
+++ src/main/java/br/com/castgroup/documentverification/utils/VerifyDocumentUtil.java
@@ -1,14 +1,20 @@
 package br.com.castgroup.documentverification.utils;
 
-
-
+import java.util.Base64;
 
 public class VerifyDocumentUtil {
 	
 	
-		
-	
 
+	public static String decodeDataBase64(String data) {
+		Base64.Decoder decoder = Base64.getDecoder();
+		return new String (decoder.decode(data.getBytes()));
+	}
 	
+	
+	public static String resultCompareDocument(String compareDocLeft, String compareDocRight ) {
+		
+		return null;
+	}
 
 }
