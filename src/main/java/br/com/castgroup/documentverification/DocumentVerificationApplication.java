e6ff98706345e579dffe2ac92c3b839870a34120
nfbf <45438873+nfbf@users.noreply.github.com>
2020-06-02 23:18:14 -0300

    iniciando a construação da arquitetura do projeto

---
+++ src/main/java/br/com/castgroup/documentverification/DocumentVerificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.castgroup.documentverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentVerificationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/castgroup/documentverification/model/Document.java
@@ -0,0 +1,51 @@
+package br.com.castgroup.documentverification.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Document {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long id;
+	private String docLeft;
+	private String docRight;
+
+	public Document() {
+
+	}
+
+	public Document(long id, String docLeft, String docRight) {
+		this.setId(id);
+		this.setDocLeft(docLeft);
+		this.setDocRight(docRight);
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDocLeft() {
+		return docLeft;
+	}
+
+	public void setDocLeft(String docLeft) {
+		this.docLeft = docLeft;
+	}
+
+	public String getDocRight() {
+		return docRight;
+	}
+
+	public void setDocRight(String docRight) {
+		this.docRight = docRight;
+	}
+
+}
